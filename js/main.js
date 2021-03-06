b0945a8216b82fdb25bbb4466b0da8ca19267d20
jdelacruzv <jldlcv@gmail.com>
2021-03-05 20:40:35 -0500

    Virtual store section was added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // ---------------> Efecto menu hamburguesa --------------->
 
-let btn_menu = document.querySelector('.btn-menu');
-let menu_items = document.querySelector('.menu-items');
-let menu_links = document.querySelectorAll('a.menu-link');
+let btn_hamburger = document.querySelector('.btn-hamburger');
+let menu = document.querySelector('.menu');
+let menu_links = document.querySelectorAll('a.menu__link');
 
-btn_menu.addEventListener('click', () => {
-    btn_menu.classList.toggle('active');
-    menu_items.classList.toggle('show');
+btn_hamburger.addEventListener('click', () => {
+    btn_hamburger.classList.toggle('active');
+    menu.classList.toggle('show');
 });
 
 // El menu desaparece cuando se hace click a cada enlace
 menu_links.forEach(link => {
     link.addEventListener('click', () => {
-        menu_items.classList.remove('show');
-        btn_menu.classList.toggle('active');
+        menu.classList.remove('show');
+        btn_hamburger.classList.toggle('active');
     });
 });
